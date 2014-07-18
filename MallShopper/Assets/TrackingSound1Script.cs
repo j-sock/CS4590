8b885df7aed1316fbff406be756f49385c1ce80f
AshleyJennings <ashleyjennings@Ashleys-MacBook-Pro.local>
2014-07-18 14:40:25 -0400

    starting on radar script, added three flute sounds

---
+++ MallShopper/Assets/TrackingSound1Script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackingSound1Script : MonoBehaviour {
+	public float distanceFromTarget;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		distanceFromTarget = Vector3.Distance(transform.position,target.position);
+		Debug.Log ("current distance from target is: " + distanceFromTarget);
+	}
+}
