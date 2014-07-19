d64455c2b952f938d8a929c3c360bf0fcb1a1e4a
AshleyJennings <ashleyjennings@Ashleys-MacBook-Pro.local>
2014-07-18 22:52:19 -0400

    You can now switch between radar objects by pressing the y key... there are three objects total

--- MallShopper/Assets/TrackingSound1Script.cs
+++ MallShopper/Assets/TrackingSound1Script.cs
@@ -6,37 +6,46 @@ public class TrackingSound1Script : MonoBehaviour {
 	public AudioClip radarSoundAbove;
 	public AudioClip radarSoundBelow;
 
+	//public GameObject targetWeAreLookingAt;
+
 	public float timeWait;
 	public float distanceFromTarget;
-	public Transform target;
+	public Transform[] targetArray;
 	bool keepPlaying;
 	bool isOnSecondFloor;
-	bool isOnFirstFloor;
+	public int currentTarget;
 
 	public bool targetOnFloorTwo;
+	//get the information from the targetScript
+	private targetScript thisTarget;
 
 
 	// Use this for initialization
 	void Start () {
+		currentTarget = 0;
 		timeWait = 3;
 		keepPlaying = true;
 		StartCoroutine(SoundOut());
 		//figure out which floor the target is on...
 		//if targetOnFloorTwo is false, target is on first floor
 		//if targetOnFloorTwo is true, target is on second floor
-		targetOnFloorTwo = true;
+
+		//get the information from the target gameobject
+		thisTarget = targetArray [currentTarget].GetComponent<targetScript>();
+		targetOnFloorTwo = thisTarget.isTargetOnFloorTwo;
+		Debug.Log ("target is on floor 2 = " + targetOnFloorTwo);
 	}
 	
 	// Update is called once per frame
 	void Update()
 	{
 				//get the distance between the user and the target item
-				distanceFromTarget = Vector3.Distance (transform.position, target.position);
+				distanceFromTarget = Vector3.Distance (transform.position, targetArray[currentTarget].position);
 				//Debug.Log ("distance from target = " + distanceFromTarget);
 
 				//get the direction the user is facing based on where the object is.
 		
-				var relativePos = target.position - transform.position;
+				var relativePos = targetArray[currentTarget].position - transform.position;
 		
 				var forward = transform.forward;
 				var angle = Vector3.Angle (relativePos, forward);
@@ -49,61 +58,67 @@ public class TrackingSound1Script : MonoBehaviour {
 		else if ((Vector3.Cross (forward, relativePos).y > 20)){
 			//Debug.Log ("target is on the right. Angle is: " + Vector3.Cross (forward, relativePos).y);
 		}
-
+		//if player presses "y", switch to the next targetItem
+		if (Input.GetKeyUp(KeyCode.Y)){
+			switchTarget();
+		}
 			
 		}
 
 	IEnumerator SoundOut()
 	{
-		while (keepPlaying){
+		while (keepPlaying) {
 			
-			//affect the Pitch of the Sound first
-			//this line is here so that the sound doesn't change while it's playing, 
-			//because that sounded horrible and offkey
-			audio.pitch = 2.0f + (-distanceFromTarget/200.0f);
+						//affect the Pitch of the Sound first
+						//this line is here so that the sound doesn't change while it's playing, 
+						//because that sounded horrible and offkey
+						audio.pitch = 2.0f + (-distanceFromTarget / 200.0f);
 
-			//determine if sound is above, below, or on the same level as the user
-			//then actually play the sound
-			//...sorry about all the if statements....
-			if((isOnFirstFloor == true) && (targetOnFloorTwo == false))
+						//determine if sound is above, below, or on the same level as the user
+						//then actually play the sound
+						//...sorry about all the if statements....
+						if ((isOnSecondFloor == false) && (targetOnFloorTwo == false)) {
 			//if the user is on the first floor and the target is on the first floor
-			{
-				audio.PlayOneShot(radarSoundLevel);
-			}
-			else if((isOnSecondFloor == true) && (targetOnFloorTwo == true))
+								audio.PlayOneShot (radarSoundLevel);
+						} else if ((isOnSecondFloor == true) && (targetOnFloorTwo == true)) {
 				//if the user is on the second floor and the target is on the second floor
-			{
-				audio.PlayOneShot(radarSoundLevel);
-			}
-			else if((isOnSecondFloor == true) && (targetOnFloorTwo == false))
+								audio.PlayOneShot (radarSoundLevel);
+						} else if ((isOnSecondFloor == true) && (targetOnFloorTwo == false)) {
 				//if the user is on the second floor and the target is on the first floor
-			{
-				audio.PlayOneShot(radarSoundBelow);
-			}
-			else if((isOnFirstFloor == true) && (targetOnFloorTwo == true))
+								audio.PlayOneShot (radarSoundBelow);
+						} else if ((isOnSecondFloor == false) && (targetOnFloorTwo == true)) {
 				//if the user is on the first floor and the target is on the second floor
-			{
-				audio.PlayOneShot(radarSoundAbove);
+								audio.PlayOneShot (radarSoundAbove);
+						}
+						//Debug.Log("sound Played (same level)");
+						yield return new WaitForSeconds (timeWait);
+		
+				}
 			}
-			//Debug.Log("sound Played (same level)");
-			yield return new WaitForSeconds(timeWait);
-		}
-	}
 
 
 	void OnTriggerEnter(Collider other) {
 		//determine which floor the user is on
 		if (other.name == "secondFloor") {
 			Debug.Log("Entered Second Floor");	
-			isOnFirstFloor = false;
 			isOnSecondFloor = true;
 		}
 		else if (other.name == "firstFloor") {
 			Debug.Log("Entered First Floor");
-			isOnFirstFloor = true;
 			isOnSecondFloor = false;
 		}
 	}
+
+	void switchTarget(){
+		//move the target up one and get the new information from it about its location
+			currentTarget = currentTarget + 1;
+			if (currentTarget >= targetArray.Length){
+				currentTarget = 0;
+			}
+				thisTarget = targetArray [currentTarget].GetComponent<targetScript>();
+				targetOnFloorTwo = thisTarget.isTargetOnFloorTwo;
+				Debug.Log ("CurrentTarget is " + currentTarget + ". This is the " + targetArray[currentTarget].gameObject.name + " and floorTwo is " + targetOnFloorTwo);
+		}
 }
 
 
---
+++ MallShopper/Assets/targetScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class targetScript : MonoBehaviour {
+	public bool isTargetOnFloorTwo;
+
+	void OnTriggerEnter(Collider other) {
+		//determine which floor the user is on
+		if (other.name == "secondFloor") {
+			isTargetOnFloorTwo = true;
+		}
+		else if (other.name == "firstFloor") {
+			isTargetOnFloorTwo = false;
+		}
+	}
+
+}
