3f54430bd84a22274b63fbfc76d5b0a929632a3c
AshleyJennings <ashleyjennings@Ashleys-MacBook-Pro.local>
2014-07-19 20:52:57 -0400

    there is now background music in all of the stores

---
+++ MallShopper/Assets/musicTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicTrigger : MonoBehaviour {
+
+	public AudioClip thisMusic;
+	
+	void Start()
+	{
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.name == "User")
+		{
+			audio.clip = thisMusic;
+			audio.Play();
+
+		}
+	}
+	
+	
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.name == "User")
+		{
+			audio.Pause();
+	
+		}
+}
+}
