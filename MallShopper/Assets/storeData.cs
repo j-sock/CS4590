c3cb3c880fe6b17006b4eeb102c9a16a64a356a7
j-sock <halvblodvarg@gmail.com>
2014-07-21 21:41:44 -0400

    changed store data and made objects transparent

--- MallShopper/Assets/storeData.cs
+++ MallShopper/Assets/storeData.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 public class storeData : MonoBehaviour {
 	
 	//element 0 is if the item is at the store
-	//element 1 is if they carry your size
-	//element 2 is if the item is on sale
+	//element 1 is your demographic rating
+	//element 2 is the item's percent sale
 	//element 3 is the price of the item
 
 	public double[] tShirt = new double[4];
