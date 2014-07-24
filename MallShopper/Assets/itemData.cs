a85cbb840a549e75efa61df55d8e7aaaee794161
j-sock <halvblodvarg@gmail.com>
2014-07-23 22:25:29 -0400

    added item data objects

---
+++ MallShopper/Assets/itemData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class itemData : MonoBehaviour {
+	
+	public double hasItem;
+	public double demographic;
+	public double sale;
+	public double price;
+
+}
