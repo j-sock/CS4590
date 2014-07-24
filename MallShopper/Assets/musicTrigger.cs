10a1061b46e70ac7b70cda077219369c135f6d94
AshleyJennings <ashleyjennings@Ashleys-MacBook-Pro.local>
2014-07-24 16:01:50 -0400

    fixed and sounds for triggers on stores

--- MallShopper/Assets/musicTrigger.cs
+++ MallShopper/Assets/musicTrigger.cs
@@ -14,7 +14,7 @@ public class musicTrigger : MonoBehaviour {
 		if(other.gameObject.name == "User")
 		{
 			audio.clip = thisMusic;
-			audio.volume = 0.8f;
+			//audio.volume = 0.8f;
 			audio.Play();
 
 		}
